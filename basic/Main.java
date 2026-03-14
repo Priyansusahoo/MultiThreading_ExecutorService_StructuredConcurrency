8045675e2ddbe698a98bc7ed6b4f20cb14d3525f
Priyansu Sahoo <priyansusahoo1@gmail.com>
2026-03-14 18:42:07 +0530

    .

---
+++ basic/Main.java
@@ -0,0 +1,38 @@
+class Main {
+    public static void main(String[] args) {
+        Runnable myRunnable = () -> {
+            for (int i = 0; i < 50; i++) {
+                if (i % 2 != 0) {
+                    System.out.println("Runnable interface - run method executing : ODD " + i);
+                }
+            }
+        };
+        
+        CustomThread t2 = new CustomThread();
+        t2.start();
+        Thread t1 = new Thread(myRunnable);
+        t1.start();
+        try {
+            t2.interrupt();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class CustomThread extends Thread {
+    public void run(){
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Thread class - run method executing : EVEN " + i);
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+                System.out.println(Thread.currentThread().getName() + " was interrupted");
+                ie.printStackTrace();
+                return;
+            }
+        }
+    }
+}
